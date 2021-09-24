a4ca21bfe77e5f5eb24da4b232813e30f4c06830
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-24 10:35:40 +0200

    Fun AIMD experimenting

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,29 @@
 			</label>
 		</div>
 		<DiaryForm v-show="!hideForm" @add="addDiaryEntry" />
+		<DiaryEntries />
+		<button v-on:click="myFunc">button w {{ buttonValue }}</button>
+		<div v-if="buttonValueHistory.length > 0">
+			<div
+				v-for="number in buttonValueHistory"
+				:key="number + '_key'"
+				:style="getWrapperStyle()"
+			>
+				<div :style="getStyle(number)"></div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import DiaryForm from './components/DiaryForm.vue';
+import DiaryEntries from './components/DiaryEntries.vue';
 
 export default {
 	name: 'App',
 	components: {
 		DiaryForm,
+		DiaryEntries,
 		// ToggleShowForm,
 	},
 	methods: {
@@ -27,11 +40,38 @@ export default {
 			console.log('diaryEntries: ', this.diaryEntries);
 			console.log('diaryEntries: ', typeof this.diaryEntries);
 		},
+		myFunc() {
+			if (this.buttonValue < 630) {
+				this.buttonValue = this.buttonValue * 2 + 7;
+			} else {
+				this.buttonValue = parseInt(this.buttonValue / 5, 10);
+			}
+			this.buttonValueHistory.unshift(this.buttonValue);
+			console.log('buttonValueHistory: ', this.buttonValueHistory);
+		},
+		getWrapperStyle() {
+			return `
+				width: 100%;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			`;
+		},
+		getStyle(num) {
+			return `
+				width: ${num}px;
+				min-width: 20px;
+				height: 20px;
+				background-color: hsl(${num * 3}deg, 50%, 50%);
+			`;
+		},
 	},
 	data() {
 		return {
 			diaryEntries: [],
+			buttonValueHistory: [],
 			hideForm: true,
+			buttonValue: 0,
 		};
 	},
 };
---
+++ src/components/DiaryEntries.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		Inside
+		<div>Diary entries!</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DiaryEntries',
+	props: {
+		entries: Array,
+	},
+};
+</script>
+
+<style scoped>
+div {
+	border: 1px solid black;
+}
+</style>
